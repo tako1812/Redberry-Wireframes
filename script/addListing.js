2682aa7055614280259d77d5755904a5e9f54920
tako1812 <troba12@freeuni.edu.ge>
2024-09-18 06:05:28 +0400

    wip:work on validation functionality

--- script/addListing.js
+++ script/addListing.js
@@ -1,17 +1,87 @@
 "use strict";
 
 const addListingBtn = document.querySelector(".add-listing-btn");
-const InputAddress = document.querySelector(".input-address");
+const inputAddress = document.querySelector(".input-address");
 const validAddress = document.querySelector(".valid-address");
+const listingDescription = document.querySelector('.listing-description');
+const validDescription = document.querySelector('.valid-description'); 
+const validPostcode = document.querySelector('.valid-postcode'); 
+const validPrice = document.querySelector('.valid-price'); 
+const validArea = document.querySelector('.valid-area');
+const validBedroomsNum = document.querySelector('.valid-bedrooms-number'); 
+const postcodeNum = document.querySelector(".postcode");
+const listingPrice = document.querySelector(".listing-price");
+const listingArea = document.querySelector(".listing-area");
+const bedroomsNumber = document.querySelector(".bedrooms-number");
 
+
+//
 const validInputs = (input) => input.length >= 2;
-addListingBtn.addEventListener("click", function () {
-  const address = InputAddress.value;
+const requiredInput = (...inputs) =>inputs.every(inp => inp !== " ");      //(a == null) !!!!!
+const validWordsAmount = (...inputs) => inputs.length >= 5;
+const validNumber = (...inputs) => inputs.every(inp=> Number.isFinite(inp));
+//
+//
+const checkAddressValidation = function () {
+  const address = inputAddress.value;
 
   if (validInputs(address)) {
     validAddress.classList.add("is-valid");
   }
-  if (!validInputs(address)) {
+  if (!validInputs(address) || !requiredInput(address)) {
+    inputAddress.classList.add("not-valid-input");   //!!!!
     validAddress.classList.add("not-valid");
   }
-});
+}
+//
+const checkDescriptionValidation = function () {
+  const description = listingDescription.value;
+
+  if (validWordsAmount(description)) {
+    validDescription.classList.add("is-valid");
+  };
+  if (!validWordsAmount(description) || !requiredInput(description)) {
+    validDescription.classList.add("not-valid");
+    listingDescription.classList.add("not-valid-input");
+  };
+}
+
+const checkNumberValidation = function() {
+  const postcode = +postcodeNum.value;
+  const price = +listingPrice.value;
+  const area = +listingArea.value;
+  const bedrooms = +bedroomsNumber.value;
+
+  if (validNumber(postcode,price, area, bedrooms)) {
+    validPostcode.classList.add("is-valid");
+    validPrice.classList.add("is-valid");
+    validArea.classList.add("is-valid");
+    validBedroomsNum.classList.add("is-valid");
+  }
+  if (!validNumber(postcode,price, area, bedrooms)) {
+    validPostcode.classList.add("not-valid");
+    validPrice.classList.add("not-valid");
+    validArea.classList.add("not-valid");
+    validBedroomsNum.classList.add("not-valid");
+
+  }
+  
+  if (!requiredInput(postcode,price, area, bedrooms)) {  !!!!!
+    postcodeNum.classList.add("not-valid-input");
+    listingPrice.classList.add("not-valid-input");
+    listingArea.classList.add("not-valid-input");
+    bedroomsNumber.classList.add("not-valid-input");
+  }
+
+}
+
+
+
+
+
+addListingBtn.addEventListener("click", checkAddressValidation);
+addListingBtn.addEventListener("click", checkDescriptionValidation);
+addListingBtn.addEventListener("click", checkNumberValidation);
+
+
+
